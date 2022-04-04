d7605614614b63e7eaa026587d420f25582dacf3
chiminhtathoj <vdlong2009@gmail.com>
2022-04-04 17:18:00 +0700

    connect data base mongoees

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+require('dotenv').config();
+
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
+
+mongoose.connect(process.env.MONGO_URL)
+.then(console.log("connected to mongoDB"))
+.catch((err)=>console.log(err));
 
 const PORT = 5000;
-console.log("test cai")
 
 app.listen(PORT, () => {
     console.log(`server running at port ${PORT}...`)
